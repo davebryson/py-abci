3a6e56d1de63fdbb0646505e8abc043d52ab530c
davebryson <dbryson@mitre.org>
2017-10-25 08:51:45 -0500

    small cleanup

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License'
+        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3.6',
     ],
     keywords='blockchain tendermint abci',
